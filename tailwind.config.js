f3d373ee6cf748018499cf9acb66486fb2d228ba
LucidMach <nukalasuraj7@gmail.com>
2022-01-03 23:52:10 +0530

    auth

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // pri: "#23eee2",
+        // sec: "#b05883",
+      },
+      fontFamily: {
+        // orbitron: ['"Orbitron"', "san-serif"],
+        // poppins: ['"Poppins"', "san-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
