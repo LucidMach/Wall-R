48638604896337b193d19f8c8273d0c2c43d7a68
LucidMach <nukalasuraj7@gmail.com>
2022-01-21 00:38:16 +0530

    graphql server initializations

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,14 +1,33 @@
-import { ApolloServer } from "apollo-server-micro";
+import { gql, ApolloServer } from "apollo-server-micro";
 
 import { typeDefs } from "./schemas";
 import { resolvers } from "./resolvers";
 
 const server = new ApolloServer({ typeDefs, resolvers });
+const startServer = server.start();
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await server.createHandler({ path: "/api/graphql" })(req, res);
+}
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-export default server.createHandler({ path: "/api/graphql" });
